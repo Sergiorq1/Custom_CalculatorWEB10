95c4203028e99a595898e08152bb1aa493bf5e07
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-10-08 14:17:36 -0700

    Updates Variables and Fixed CSS

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ let btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
     let years = parseFloat(document.getElementById('years').value);
-    let tipPercentage = parseFloat(document.getElementById('tip_percentage').value);
-    let people = parseFloat(document.getElementById('people').value);
+    let months = parseFloat(document.getElementById('months').value);
+    let days = parseFloat(document.getElementById('days').value);
 
-    let computedTip = years / tipPercentage;
-    let totalBill = document.getElementById('total_bill').value = years / people + computedTip / people;
-    let tipAmount = document.getElementById('tip_amount').value = computedTip;
+    let daysAlive = document.getElementById('days-alive').value =
+    let secondsAlive = document.getElementById('seconds-alive').value = 
 })
