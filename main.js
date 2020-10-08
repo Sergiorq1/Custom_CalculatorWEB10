3554c54868a495274e02c9e9a40fb12395d43063
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-10-08 14:39:52 -0700

    Finished

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ btn.addEventListener('click', function(){
     let months = parseFloat(document.getElementById('months').value);
     let days = parseFloat(document.getElementById('days').value);
 
-    let daysAlive = document.getElementById('days-alive').value =
-    let secondsAlive = document.getElementById('seconds-alive').value = 
+    let daysAlive = document.getElementById('days-alive').value = (years*365) + (months*12) + days
+    let secondsAlive = document.getElementById('seconds-alive').value = (years*31,536,000) + (months*2,592,000) + (days*86400)
 })
